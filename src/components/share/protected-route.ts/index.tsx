04fd53e21bcdc5f8feafe06b36e4dfeb27801d61
Quoc Thang <comehere.thang@gmail.com>
2024-07-15 13:40:19 +0700

    Fix bugs kiểm tra quyền hạn truy cập vào trang quản trị
    Fix bugs kiểm tra quyền hạn truy cập vào trang quản trị

--- src/components/share/protected-route.ts/index.tsx
+++ src/components/share/protected-route.ts/index.tsx
@@ -4,15 +4,17 @@ import NotPermitted from './not-permitted'
 import Loading from '../loading'
 
 const PermissionBaseRoute = (props: any) => {
-  const user = useAppSelector((state) => state.account.user)
-  // const unionist = useAppSelector((state) => state.account.unionist)
-  const userPermission = user.permissions[0].method
-  // const unionistPermission = unionist.permissions[0].method
+  const account = useAppSelector((state) => state.account)
+  const user = account?.user
+  const unionist = account?.unionist
+  const userHasPermission = user?.permissions?.some(
+    (permission) => permission.method === 'ACCESS_TO_ADMIN_PAGE'
+  )
+  const unionistHasPermission = unionist?.permissions?.some(
+    (permission) => permission.method === 'ACCESS_TO_ADMIN_PAGE'
+  )
 
-  if (
-    userPermission === 'ACCESS_TO_ADMIN_PAGE'
-    // unionistPermission !== 'ACCESS_TO_ADMIN_PAGE'
-  ) {
+  if ((user && userHasPermission) || (unionist && !unionistHasPermission)) {
     return <>{props.children}</>
   } else {
     return <NotPermitted />
