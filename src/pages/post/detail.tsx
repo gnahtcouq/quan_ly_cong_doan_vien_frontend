b49f428721df44b6a51ce88c2d1b0d0fcfea5e3e
Quoc Thang <comehere.thang@gmail.com>
2024-08-03 22:38:40 +0700

    Fix lỗi ngôn ngữ
    Fix lỗi ngôn ngữ

--- src/pages/post/detail.tsx
+++ src/pages/post/detail.tsx
@@ -13,7 +13,7 @@ import 'quill/dist/quill.snow.css'
 import logo_cd from '@/assets/logo-cd.png'
 
 dayjs.extend(relativeTime)
-dayjs.locale('vi_VN')
+dayjs.locale('en')
 
 const ClientPostDetailPage = (props: any) => {
   const [postDetail, setPostDetail] = useState<IPost | null>(null)
